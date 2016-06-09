b1a4973df8d52a385a15f75c16aa75c5c7f4a28f
anders462 <anders462@gmail.com>
2016-06-08 17:47:40 -0700

    client base url change

--- public/app/core/constants.js
+++ public/app/core/constants.js
@@ -3,10 +3,10 @@
 'use strict'
 
 angular.module('SenseIt.core')
-  .constant("BASE_URL", "http://192.168.1.70:8001/")
+  .constant("BASE_URL", "https://sense-it.herokuapp.com/")
 
 
 })();
 
-
+//DEV http://192.168.1.70:8001/
 //https://sensit.herokuapp.com/
