7abdbea67fd9d527bfcedf24e7638e4c1241e0a1
anders462 <anders462@gmail.com>
2016-06-08 19:31:32 -0700

    client base url change

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': process.env.SECRET_KEY,
-    'mongoUrl' : process.env.MONGO_URI,
+    'mongoUrl' : process.env.MONGODB_URI,
     'facebook': {
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
